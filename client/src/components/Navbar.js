96881f3a41746ca18ff620ab82c025cd0bd1749b
Mervin Abagat <mervin@ambercreative.sg>
2018-11-21 18:56:33 +0800

    first commit

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Box, Text, Heading, Image, Avatar, Button, Card, Checkbox, Collage, Column, Container, Divider, Flyout, GroupAvatar, Icon, IconButton, Label, Layer, Letterbox, Link, Mask, Masonry, Modal, Pog, Pulsar, RadioButton, SearchField, SegmentedControl, SelectList, Spinner, Sticky, Switch, Tabs, TextArea, TextField, Toast, Tooltip, Touchable, Video } from 'gestalt';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => (
+   <Box
+    height={70}
+    color="midnight"
+    padding={1}
+    shape="roundedBottom"
+    display="flex"
+    alignItems="center"
+    justifyContent="around"
+   >
+    <NavLink activeClassName="active" to="/signin">
+        <Text size="xl" color="white">Sign In</Text>
+    </NavLink>
+    
+    <NavLink activeClassName="active" exact to="/">
+    <Box display="flex" alignItems="center" >
+        <Box margin={2} height={50} width={50}>
+            <Image alt="Strapi" naturalHeight={1} naturalWidth={1} src="./icons/logo.svg" />
+        </Box>
+        <Heading size="xs" color="orange">Strapi</Heading>
+    </Box>
+    </NavLink>
+
+    <NavLink activeClassName="active" to="/signup">
+        <Text size="xl" color="white">Sign Up</Text>
+    </NavLink>
+   </Box>
+)
+
+export default Navbar;
